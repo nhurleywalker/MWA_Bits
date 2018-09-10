ea7a8fb24451d036e3d2c8dd1e96df629482bbe9
Natasha Hurley-Walker <nhw@icrar.org>
2018-09-10 15:24:25 +0800

    Added boilerplate so script can be called from other python code

--- add_SUMSS_beam.py
+++ add_SUMSS_beam.py
@@ -1,21 +1,38 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 
 import sys
 from astropy.io import fits
 from numpy import sin, radians
+import argparse
 
-hdu = fits.open(sys.argv[1])
+def add_sumss_beam(fitsfile, outname):
+    hdu = fits.open(fitsfile)
+    Dec = hdu[0].header["CRVAL2"]
 
-Dec = hdu[0].header["CRVAL2"]
+    bpa = 0.0
+    bmin = 45./3600.
+    bmaj = bmin / sin(abs(radians(Dec)))
 
-bpa = 0.0
-bmin = 45./3600.
+    hdu[0].header["BMAJ"] = bmaj
+    hdu[0].header["BMIN"] = bmin
+    hdu[0].header["BPA"] = bpa
 
-bmaj = bmin / sin(abs(radians(Dec)))
+    if outname is None:
+        outname = fitfile.replace(".fits", "_wpsf.fits")
+    hdu.writeto(outname)
 
-hdu[0].header["BMAJ"] = bmaj
-hdu[0].header["BMIN"] = bmin
-hdu[0].header["BPA"] = bpa
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    group1 = parser.add_argument_group("Input/output files")
+    group1.add_argument("--infits", dest='infits', type=str, default=None,
+                        help="The fits image to add a beam to.")
+    group1.add_argument("--outfits", dest='outfits', type=str, default=None,
+                        help="The output filename (default = _wpsf)."
+    results = parser.parse_args()
 
-hdu.writeto(sys.argv[2])
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        sys.exit()
+
+    add_sumss_beam(results.infits, results.outfits)
 
