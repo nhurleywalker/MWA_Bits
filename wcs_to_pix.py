5da85fc7361590958cbe6bcce1ccc197344a84a1
Dr Natasha Hurley-Walker <nhw@icrar.org>
2019-03-14 12:29:26 +0800

    Bugfixes

--- wcs_to_pix.py
+++ wcs_to_pix.py
@@ -8,10 +8,10 @@ from astropy import wcs
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-def wcs_to_pix(ra, dec, fitsimage):
+def wcs_to_pix(ra=None, dec=None, indexing=0, fitsimage=None):
     hdu = fits.open(fitsimage)
-    w = wcs.WCS(hdu[0].header)
-    x, y = w.wcs_world2pix([[ra,dec]],0).transpose()
+    w = wcs.WCS(hdu[0].header, naxis=2)
+    x, y = w.wcs_world2pix([[ra,dec]],indexing).transpose()
 #    if hdu[0].header["CTYPE1"] == "GLON":
 #        coords = SkyCoord(lon, lat, frame="galactic", unit = (u.deg, u.deg))
 #    else:
@@ -23,8 +23,10 @@ if __name__ == "__main__":
     group1 = parser.add_argument_group("Input/output files")
     group1.add_argument("--ra", dest='ra', type=float, default=None,
                         help="RA / longitude value (deg)")
-    group1.add_argument("--dec", dest='outfits', type=float, default=None,
+    group1.add_argument("--dec", dest='dec', type=float, default=None,
                         help="Dec / latitude value (deg)")
+    group1.add_argument("--indexing", dest='indexing', type=int, default=0,
+                        help="WCS indexing to use (default = 0)")
     group1.add_argument("--fitsimage", dest='fitsimage', type=file, default=None,
                         help="Fits image to use")
     results = parser.parse_args()
@@ -33,6 +35,6 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit()
 
-    x, y = wcs_to_pix(ra, dec, fitsimage)
+    x, y = wcs_to_pix(results.ra, results.dec, results.indexing, results.fitsimage)
     print x, y
 
