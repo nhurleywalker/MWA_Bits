eec3c9d81c558a19b50526071b86009987302a64
Natasha Hurley-Walker <nhw@icrar.org>
2020-04-25 04:42:54 +0800

    Added author info, first stab at parallelising

--- moving_median_continuum_subtraction.py
+++ moving_median_continuum_subtraction.py
@@ -6,14 +6,50 @@ from astropy.io import fits
 import argparse
 import sys
 import numpy as np
+import psutil
+# Parallelise the code
+import multiprocessing
+# multiple cores support
+import pprocess
+
+__author__ = ["Natasha Hurley-Walker"]
+__date__ = "2020-04-24"
+
+def _fslice_median(args):
+    """
+    A shallow wrapper for slice_median
+
+    Parameters
+    ----------
+    args : list
+        A list of arguments for slice_median
+
+    Returns
+    -------
+    None
+    """
+    # an easier to debug traceback when multiprocessing
+    # thanks to https://stackoverflow.com/a/16618842/1710603
+    try:
+        return slice_median(*args)
+    except:
+        import traceback
+        raise Exception("".join(traceback.format_exception(*sys.exc_info())))
+
+
+def slice_median(index, i, j, data):
+    median = np.nanmedian(data[:,i:j,:,:], axis=1)
+    return index, median
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    group1 = parser.add_argument_group("Input/output files")
+    group1 = parser.add_argument_group("Options")
     group1.add_argument("--fitsfile", dest='fitsfile', type=str, default=None,
                         help="Name of the input cube to be continuum-subtracted.")
     group1.add_argument("--binwidth", dest='binwidth', type=float, default=20,
                         help="Number of channels in each bin (default = 20).")
+    group1.add_argument('--cores', dest='cores', default=None, type=int,
+                        help="Number of cores to use (default = autodetect")
     options = parser.parse_args()
 
     if len(sys.argv) <= 1 or options.fitsfile is None:
@@ -25,20 +61,53 @@ if __name__ == "__main__":
     contfile = infile.replace(".fits", "_moving_median.fits")
     binwidth = options.binwidth
 
+    if results.cores is None:
+        cores = multiprocessing.cpu_count()
+    else:
+        cores = results.cores
+
     hdu = fits.open(infile)
     data = hdu[0].data
     nchans = data.shape[1] #0 pol, 2 Dec, 3 RA
 # Replace Slice 55 with a copy of Slice 56 to avoid having to use nanmedian
-    data[:,54,:,:] = data[:,55,:,:]
+#    data[:,54,:,:] = data[:,55,:,:]
 
     med = np.zeros(hdu[0].data.shape)
 
-    # Calculate the moving average
-    # TODO: thread
-    # TODO: implement step sizes >1
-    # TODO: re-implement nanmedian with speedups
+    # We need to order our arguments by an index, since the results will be returned
+    # in whatever order the pooled tasks finish
+    n = 0
+    # TODO: Check this doesn't blow up memory usage; I may have to slice
     for i in range(binwidth/2, nchans - binwidth/2):
-        med[:,i,:,:] = np.median(data[:,i-binwidth/2:i+binwidth/2,:,:], axis=1)
+        args.append((n, i-binwidth/2, i+binwidth/2, data))
+        n+=1
+
+#    # start a new process for each median
+    pool = multiprocessing.Pool(processes=cores, maxtasksperchild=1)
+    try:
+        # chunksize=1 ensures that we only send a single task to each process
+        results = pool.map_async(_fslice_median, args, chunksize=1).get(timeout=10000000)
+    except KeyboardInterrupt:
+        pool.close()
+        sys.exit(1)
+    pool.close()
+    pool.join()
+
+#    for i in range(binwidth/2, nchans - binwidth/2):
+#        med[:,i,:,:] = np.median(data[:,i-binwidth/2:i+binwidth/2,:,:], axis=1)
+
+    indices, medianss = map(list, zip(*results))
+    # Order correctly
+    ind = np.argsort(indices)
+    medians = np.array(medians)
+    print(medians.shape)
+    medians = medians[ind]
+    print(medians.shape)
+#    # Flatten list of lists
+#    o = [item for sublist in offsets for item in sublist]
+#    # Make into array and apply
+#    x += np.array(o)
+
 
     # Handle the edges with a single median across (half the binwidth) channels
     # TODO: Use the right numpy function (tile?) to make this easier to read
@@ -51,7 +120,7 @@ if __name__ == "__main__":
         med[:,i,:,:] = med_end
 
     # Re-flag the central channel
-    data[:,54,:,:] = np.nan*np.ones(data[:,54,:,:].shape)
+#    data[:,54,:,:] = np.nan*np.ones(data[:,54,:,:].shape)
     hdu[0].data = data - med
     hdu.writeto(outfile)
     hdu[0].data = med 
