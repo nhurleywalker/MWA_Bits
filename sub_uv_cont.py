394d2c412b8330800989ce9c9f479b6c8c4aa940
Natasha Hurley-Walker <nhw@icrar.org>
2020-05-15 12:39:37 +0800

    Command-line options and boxcar averaging

--- sub_uv_cont.py
+++ sub_uv_cont.py
@@ -3,33 +3,69 @@
 import sys
 import numpy as np
 from casacore.tables import table
+import argparse
 
-obsid = sys.argv[1]
+__author__ = "Natasha Hurley-Walker"
+__date__ = "2020-05-01"
 
-mset = table("{0}.ms".format(obsid), readonly=True)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    group1 = parser.add_argument_group("Input/output files")
+    group1.add_argument("--msfile", dest='msfile', type=str, default=None,
+                        help="Name of the input visibilities to be continuum-subtracted.")
+    group1.add_argument("--datacolumn", dest='datacolumn', type=str, default="CORRECTED_DATA",
+                        help="Column to subtract from (default = CORRECTED_DATA).")
+    group1.add_argument("--binwidth", dest='binwidth', type=int, default=16,
+                        help="Number of channels in each bin (default = 16).")
+    options = parser.parse_args()
 
-data = mset.getcol("CORRECTED_DATA")
-flag = mset.getcol("FLAG")
-#real = ms.getdata("real")["real"]
-#imag = ms.getdata("imaginary")["imaginary"]
-#flag = ms.getdata("flag")["flag"]
-#weight = ms.getdata("weight")["weight"]
-#mreal = np.ma.masked_array(real, mask=flag)
-mdata = np.ma.masked_array(data, mask=flag)
+    if len(sys.argv) <= 1 or options.msfile is None:
+        parser.print_help()
+        sys.exit()
 
-# Assume channel widths of 24
-chanwidth = 24
+    binwidth = options.binwidth
+    msfile = options.msfile
 
-for start in np.arange(0, data.shape[1], chanwidth):
-    end = start + chanwidth
-    avg = np.ma.mean(mdata[:,start:end,:], axis=1)
-    avg_rep = np.repeat(avg[:, np.newaxis, :], chanwidth, axis=1)
-    data[:,start:end,:] = data[:,start:end,:] - avg_rep
+    mset = table("{0}".format(options.msfile), readonly=True)
+    # Have to make a copy first
+    mset_sub = mset.copy(msfile.replace(".ms", "_sub.ms"), deep = True)
 
-# Have to make a copy first
-mset_sub = mset.copy("{0}_sub.ms".format(obsid), deep = True)
-mset_sub = table("{0}_sub.ms".format(obsid), readonly = False)
-mset_sub.putcol("CORRECTED_DATA", data)
+    data = mset.getcol(options.datacolumn)
+    flag = mset.getcol("FLAG")
+    nchans = data.shape[1] #0 pol, 2 time and baseline?
 
-mset.close()
-mset_sub.close()
+    ## There are also values set to NaN that are not in the flag table
+    new_flags = np.isnan(data)
+    # If the data is NaN OR the data are flagged then we want to flag it
+    total_flags = np.logical_or(flag, new_flags)
+
+    mdata = np.ma.masked_array(data, mask=total_flags)
+
+    avg = np.zeros(mdata.shape, dtype="complex")
+
+    # Calculate the moving average
+    for i in range(binwidth/2, nchans - binwidth/2):
+        avg[:,i,:] = np.ma.mean(mdata[:,i-binwidth/2:i+binwidth/2,:], axis=1)
+
+    # Handle the edges with a single mean across (half the binwidth) channels
+    avg_start = np.ma.mean(mdata[:,0:binwidth/2,:], axis=1)
+    avg_end = np.ma.mean(mdata[:,nchans-binwidth/2:nchans,:], axis=1)
+    for i in range(0, binwidth/2):
+        avg[:,i,:] = avg_start
+    for i in range(nchans-binwidth/2, nchans):
+        avg[:,i,:] = avg_end
+
+    mdata = mdata - avg
+
+    mset_sub = table(msfile.replace(".ms", "_sub.ms"), readonly = False)
+    mset_sub.putcol(options.datacolumn, mdata)
+
+    mset.close()
+    mset_sub.close()
+
+# Old-fashioned binning: too many residuals
+#    for start in np.arange(0, data.shape[1], binwidth):
+#        end = start + binwidth
+#        avg = np.ma.mean(mdata[:,start:end,:], axis=1)
+#        avg_rep = np.repeat(avg[:, np.newaxis, :], binwidth, axis=1)
+#        data[:,start:end,:] = data[:,start:end,:] - avg_rep
