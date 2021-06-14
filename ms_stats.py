953fa2b714a32f1384e2596cbc9abb9c0405ee0f
Dr Natasha Hurley-Walker <nhw@icrar.org>
2021-06-14 11:06:39 +0800

    Print length of measurement set

---
+++ ms_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import numpy as np
+import sys
+#import os
+#from datetime import datetime
+from casacore.tables import table, taql, maketabdesc, makescacoldesc
+#from astropy.time import Time
+#import astropy.units as u
+
+#Root directory
+#root = sys.argv[3]
+
+
+#Opening ms files
+print(sys.argv)
+sys.stdout.flush()
+ms = sys.argv[1]
+mset = table(ms, readonly=True)
+
+time = list(set(mset.getcol("TIME")))
+
+print(len(time))
+
+mset.close()
